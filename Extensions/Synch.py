8c73fad76343d6450882859de9fe352d6a2f37c7
jad52 <jad52@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-06-01 12:52:09 +0000

    Added doc strings to all the functions and removed global variables from the module.

--- Extensions/Synch.py
+++ Extensions/Synch.py
@@ -5,6 +5,7 @@ from Products.CMFCore.utils import getToolByName
 
 # hit the api and get the xml object
 def getRoster(courseID, apiurl, apiaction, apiuser, apipwd):
+    """Return the xml retrieved from the ANGEL API"""
     data = urllib.urlencode({"strcourse_id":courseID,"apiaction":apiaction,"apiuser":apiuser,"apipwd":apipwd})
     f = urllib.urlopen(apiurl, data)   
     thexml = f.read()
@@ -15,7 +16,6 @@ def getRoster(courseID, apiurl, apiaction, apiuser, apipwd):
 def handleRoster(self):
     """Return a dictionary of course roster as found in ANGEL"""
     #set some global vars that all functions will need
-    global regTool, addInstructors, addEditors, addStudents, context
     context = self    
     # create the prop tool to extract values from the site properties
     propTool = getToolByName(context, 'portal_properties')
@@ -50,10 +50,10 @@ def handleRoster(self):
     if errorMessage != "":
         return  errorMessage
     else: 
-        createGroups()
+        createGroups(context)
         members = src.getElementsByTagName("member")
-        roster = handleMembers(members, src, addInstructors, addEditors, addStudents)
-        createAdditionalUsers(addInstructors, addEditors, addStudents)
+        roster = handleMembers(members, src, addInstructors, addEditors, addStudents, regTool, context)
+        createAdditionalUsers(addInstructors, addEditors, addStudents, regTool, context)
         return roster
 
 # this function extracts and returns the actual text from the xml text node        
@@ -67,7 +67,7 @@ def getText(nodelist):
 #Parse the xml and create a list of user dictionaries with the keys userid, fname and lname 
 # sort the dictionaries by last name
 # while we're at it create the user if he or she is not already present
-def handleMembers(members, src, addInstructors, addEditors, addStudents):
+def handleMembers(members, src, addInstructors, addEditors, addStudents,regTool, context):
     """Create a dictionary of users found in the ANGEL roster."""
     person = {}
     userDicts = []
@@ -83,31 +83,32 @@ def handleMembers(members, src, addInstructors, addEditors, addStudents):
         userList.append(userid)
         userDicts.append(person)
         #create this user
-        createUser(userid, fname, lname, rights)
+        createUser(userid, fname, lname, rights, regTool, context)
     userDicts = sorted(userDicts, key=itemgetter('lname'))
     #delete any users who are not in ANGEL or additional users
-    deleteUsers(userList, addInstructors, addEditors, addStudents)
+    deleteUsers(userList, addInstructors, addEditors, addStudents, context)
     return userDicts
 
 # deals with an error message if one is returned from the xml  
 def handleError(messages):
+    """Return the error message received from the ANGEL API"""
     msg = ""
     for message in messages:
         msg = getText(message.childNodes)
     return msg  
 
 #inserts the users added to the Plone site who are not in ANGEL
-def createAdditionalUsers(addInstructors, addEditors, addStudents):  
+def createAdditionalUsers(addInstructors, addEditors, addStudents, regTool, context):  
     """Create users that are added to the roster via Plone configlet."""
     #import pdb; pdb.set_trace()
     for userid in addInstructors:
-        createUser(userid, '', '', '32')
+        createUser(userid, '', '', '32', regTool, context)
     for userid in addEditors:
-        createUser(userid, '', '', '16')
+        createUser(userid, '', '', '16', regTool, context)
     for userid in addStudents:
-        createUser(userid, '', '', '2')   
+        createUser(userid, '', '', '2', regTool, context)   
 
-def createUser(userid, fname, lname, rights):  
+def createUser(userid, fname, lname, rights, regTool, context):  
     """Create users and put them in the appropriate groups."""   
     if rights == '32':
         groupname = 'Instructors'
@@ -120,14 +121,14 @@ def createUser(userid, fname, lname, rights):
     if userid not in allUsers and userid != '':
         regTool.addMember(userid,'goober123','', properties={'username':userid,'email':userid+'@psu.edu','fullname': fname +' '+lname})    
     if userid not in groupMembers:
-        addUserToGroup(groupname, userid)
+        addUserToGroup(groupname, userid, context)
         
-def addUserToGroup(groupname, userid):
+def addUserToGroup(groupname, userid, context):
     group = context.portal_groups.getGroupById(groupname)    
     group.addMember(userid)
 
 # removes a user from all groups (called when a user is deleted)
-def removeUserFromAllGroups(userid):
+def removeUserFromAllGroups(userid, context):
     """Remove a user from all groups (called when a user is deleted)"""
     groups=context.portal_groups.getGroupIds()
     for group in groups:
@@ -135,7 +136,8 @@ def removeUserFromAllGroups(userid):
         groupname.removeMember(userid)
         
 # create groups if for some reason they don't exist
-def createGroups():
+def createGroups(context):
+    """Create groups required by the ANGEL API"""
     if not context.portal_groups.getGroupById('Instructors'):
         context.portal_groups.addGroup('Instructors',)
     if not context.portal_groups.getGroupById('Course Editors'):
@@ -143,7 +145,8 @@ def createGroups():
     if not context.portal_groups.getGroupById('Students'):
         context.portal_groups.addGroup('Students',)
                 
-def deleteUsers(angelUsers, addInstructors, addEditors, addStudents):
+def deleteUsers(angelUsers, addInstructors, addEditors, addStudents, context):
+    """Remove users who are not in the ANGEL API or explicitley added by the portal roster synch properties."""
     #create a list of all users to compare with site users
     #if in site users, but not in the list nuke 'em
     allUsers = angelUsers
@@ -155,6 +158,6 @@ def deleteUsers(angelUsers, addInstructors, addEditors, addStudents):
     for userid in siteUsers:
         if userid not in allUsers:
             mtool.deleteMembers(userid)
-            removeUserFromAllGroups(userid)
+            removeUserFromAllGroups(userid, context)
 
    
